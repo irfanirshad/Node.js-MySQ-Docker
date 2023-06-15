bdd1918a204b162a363dd9be9f551cb16dba2edd
Irfan <aaaii@Irfans-MacBook-Pro.local>
2023-06-15 07:28:42 +0530

    Added MySQL queries into the query folder

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ip from 'ip';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import Response from './domain/response.js';
+import logger from './util/logger.js';
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000 ; 
@@ -12,6 +13,6 @@ app.use(express.json());
 
 app.get('/', (req,res) => {
     res.send(new Response(200, 'OK', 'Patient API, v1.0.0 - All Systems Go'));
-});
+}); 
 console.log(process.env);
-app.listen(PORT, () => console.log(`Server running on: ${ip.address()}:${PORT}`));
+app.listen(PORT, () => logger.info(`Server running on: ${ip.address()}:${PORT}`));
